094a4d2764d6a95908472272f5a77e8fea3e8b3c
latenace <77824915+latenace@users.noreply.github.com>
2024-05-04 15:35:20 +1000

    Draggy

--- main.js
+++ main.js
@@ -9,3 +9,32 @@ dropZone.addEventListener('dragover', function(event) {
 dropZone.addEventListener('drop', function(event) {
 	dropZone.prepend(card)
 })
+
+
+function onDragStart(event) {
+	event
+	  .dataTransfer
+	  .setData('text/plain', event.target.id);
+  
+	event
+	  .currentTarget
+	  .style
+	  .backgroundColor = 'yellow';
+  }
+
+function onDragOver(event) {
+	event.preventDefault();
+  }
+
+function onDrop(event) {
+	const id = event
+	  .dataTransfer
+	  .getData('text');
+	  const draggableElement = document.getElementById(id);
+	  const dropzone = event.target;
+	  dropzone.appendChild(draggableElement);
+	  event
+	  	.dataTransfer
+	  	.clearData();
+	
+  }
